0bc248018ed4bb84ce0cc2da6c0021845d6be1ed
Kenny Shepard <kshepard@gmail.com>
2019-01-05 23:24:08 -0500

    Add day 9 part 2

--- src/Day09.hs
+++ src/Day09.hs
@@ -8,38 +8,47 @@ import           Text.Megaparsec.Char.Lexer
 
 type Parser = Parsec Void String
 
+noMarkerParser :: Parser Int
+noMarkerParser = length <$> some (satisfy (/= '('))
 
-inputParser :: Parser Int -> Parser Int
-inputParser markerParser = sum <$> many (markerParser <|> noMarkerParser)
-
-markerParser1 :: Parser Int
-markerParser1 = do
+lengthRepsParser :: Parser (Int, Int)
+lengthRepsParser = do
   char '('
   repLength <- decimal
   char 'x'
   numReps <- decimal
   char ')'
+  pure (repLength, numReps)
+
+marker1Parser :: Parser Int
+marker1Parser = do
+  (repLength, numReps) <- lengthRepsParser
   replicateM_ repLength anyChar
   pure $ numReps * repLength
 
-markerParser2 :: Parser Int
-markerParser2 =
-  -- TODO
-  undefined
+marker2Parser :: Parser Int
+marker2Parser = do
+  (repLength, numReps) <- lengthRepsParser
+  substr               <- replicateM repLength anyChar
+  let subResult = case parse part2Parser mempty substr of
+        Left  err -> error $ parseErrorPretty err
+        Right x   -> x
+  pure $ numReps * subResult
 
-noMarkerParser :: Parser Int
-noMarkerParser = length <$> some (satisfy (/= '('))
+part1Parser :: Parser Int
+part1Parser = sum <$> many (marker1Parser <|> noMarkerParser)
+
+part2Parser :: Parser Int
+part2Parser = sum <$> many (marker2Parser <|> noMarkerParser)
 
 decompressInput :: Parser Int -> String -> Int
-decompressInput markerParser input =
-  case parse (inputParser markerParser <* eof) mempty input of
+decompressInput partParser input =
+  case parse (partParser <* eof) mempty input of
     Left  err -> error $ parseErrorPretty err
     Right x   -> x
 
-
 main :: IO ()
 main = do
-  input <- readFile "input/09.txt"
-  let finalInput = init input
-  print $ decompressInput markerParser1 finalInput
-  print $ decompressInput markerParser2 finalInput
+  input <- init <$> readFile "input/09.txt"
+  print $ decompressInput part1Parser input
+  print $ decompressInput part2Parser input
